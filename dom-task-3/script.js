140bb86c24ad8f2c1e64de4591f5d46ac329e73a
WajahatAli8710 <wajahatali8710@gmail.com>
2025-11-18 19:51:51 +0530

    changes

--- dom-task-3/script.js
+++ dom-task-3/script.js
@@ -9,13 +9,13 @@ const text = [
 ];
 btn.addEventListener("click", () => {
   let num = Math.floor(Math.random() * text.length);
-  console.log(num);
-
   let x = Math.floor(Math.random() * 75 + 1);
-  let y = Math.floor(Math.random() * 80 - 1);
-  let deg = Math.floor(Math.random() * 40);
-  let scale = Math.floor((Math.random() * 2) +1) ;
-  console.log(scale , "scale");
+  let y = Math.floor(Math.random() * 75 - 1);
+  let deg = Math.floor(Math.random() * 180);
+  let scale = Math.floor(Math.random() * 2 + 1);
+  let c1 = Math.floor(Math.random() * 256);
+  let c2 = Math.floor(Math.random() * 256);
+  let c3 = Math.floor(Math.random() * 256);
 
   const h1 = document.createElement("h1");
   h1.innerHTML = text[num];
@@ -24,6 +24,7 @@ btn.addEventListener("click", () => {
   h1.style.left = x + "%";
   h1.style.rotate = deg + "deg";
   h1.style.scale = scale;
+  h1.style.color = `rgb(${c1} ,${c2} , ${c3})`;
 
   main.appendChild(h1);
 });
