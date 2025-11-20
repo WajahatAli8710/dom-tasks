cd32b227129c3c9f4700dbe586342d5fcee3e7bd
WajahatAli8710 <wajahatali8710@gmail.com>
2025-11-20 17:51:52 +0530

    task 4

--- dom-task-4/script.js
+++ dom-task-4/script.js
@@ -16,15 +16,16 @@ btn.addEventListener("click", function () {
 
   setTimeout(function () {
     clearInterval(load);
+    grow = 0;
+    bar.style.width = grow + "%";
+    h1.textContent = grow + "%";
+    btn.innerHTML = "Downloaded"
+    btn.style.opacity = 1;
+    btn.style.pointerEvents = "";
     const a = document.createElement("a");
     a.href = `./dom.txt`;
     a.download = "dom.txt";
     a.click();
-    grow = 0;
-    bar.style.width = grow + "%";
-    h1.textContent = grow + "%";
-    btn.style.opacity = 1;
-    btn.style.pointerEvents = "";
   }, num * 100);
 
   console.log("Download in", num / 10, "Second");
